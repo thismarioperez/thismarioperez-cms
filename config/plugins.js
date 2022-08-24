3d33e68e547e81df8175e2505244a71501f69443
Mario Perez <mario.perez415@gmail.com>
2022-08-23 21:51:23 -0400

    Install aws s3 provider for upload plugin. Update env example

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,4 @@
-module.exports = (env) => ({
+module.exports = ({ env }) => ({
   graphql: {
     enabled: true,
     config: {
@@ -9,6 +9,32 @@ module.exports = (env) => ({
       amountLimit: 200,
       apolloServer: {
         tracing: false,
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+      breakpoints: {
+        xxxlarge: 1920,
+        xxlarge: 1600,
+        xlarge: 1366,
+        large: 1024,
+        medium: 768,
+        small: 640,
       },
     },
   },
