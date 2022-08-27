2a35e56dd2d3856b648ba95d3cee8956d0be29f8
Mario Perez <mario.perez415@gmail.com>
2022-08-27 16:56:14 -0400

    Update db config to enable saving db entries with emojis

--- config/database.js
+++ config/database.js
@@ -8,6 +8,7 @@ module.exports = ({ env }) => ({
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'strapi'),
       ssl: env.bool('DATABASE_SSL', false),
+      charset: env('DATABASE_CHARSET', 'utf8mb4') // need this to enable emojis
     },
   },
 });
